b7e4223cdd1272566403c5e3fbba06930f527b3f
C1310859 Lucas Moreira Peres Barga <c1310859@interno.bb.com.br>
2020-01-03 17:45:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
-import { ThemedButton } from './components/ThemedButton';
-import { LIGHT, DARK } from './components/Style';
+import React from 'react';
+import phone from './images/iphone.png'
+// import React, { useState } from 'react';
+// import { LIGHT, DARK } from './components/Style';
 
 export const ThemeContext = React.createContext();
 
-const light = {
-  backgroundColor: LIGHT
-}
+// const light = {
+//   backgroundColor: LIGHT
+// }
 
-const dark = {
-  backgroundColor: DARK
-}
+// const dark = {
+//   backgroundColor: DARK
+// }
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  // const [theme, setTheme] = useState(light);
 
-  const changeTheme = () => {
-    setTheme( theme === light ? dark : light);
-  }
+  // const changeTheme = () => {
+  //   setTheme( theme === light ? dark : light);
+  // }
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <button onClick={() => changeTheme()}>Alterar tema</button>
-      <ThemedButton />
-    </ThemeContext.Provider>
+    // <ThemeContext.Provider value={theme}>
+    //   <button onClick={() => changeTheme()}>Alterar tema</button>
+    //   <ThemedButton />
+    // </ThemeContext.Provider>
+
+
+    <div className="section hero">
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <h4 className="hero-heading">Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
+            <a className="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+          </div>
+          <div className="one-half column phones">
+            <img className="phone" src={phone} alt="Phone" />
+            <img className="phone" src={phone} alt="Phone" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
